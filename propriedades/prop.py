9ab31c5fd8ffd26e6cbd6c13b6064eb77d89eee7
JoaoJuniorGrb <108489218+JoaoJuniorGrb@users.noreply.github.com>
2024-05-14 21:06:45 -0300

    Update prop.py

--- propriedades/prop.py
+++ propriedades/prop.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import requests
+from io import BytesIO
 
 #Inicial
 programas = ["Propriedades Termodinâmicas","Perda de Carga","Final"]
